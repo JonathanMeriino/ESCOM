9f5366760b930b0c05174ba8a42f838791125c08
JonathanBM <jbenitez.m14@gmail.com>
2020-12-16 17:10:19 -0600

    Lo mismo

--- Lista1/Problema0.c
+++ Lista1/Problema0.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
   int *u=NULL, *v=NULL, *w=0, *x=NULL, *y=NULL, *z=NULL;
-int a=2, b=3, c=5, d=7, e=11, f= 13; 
+  int a=2, b=3, c=5, d=7, e=11, f= 13;
 
   return 0;
 }
