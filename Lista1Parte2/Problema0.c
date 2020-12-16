aad1349e112afc56f5471245eec2bc57483f8f64
JonathanBM <jbenitez.m14@gmail.com>
2020-12-16 16:40:22 -0600

    Problema 0

---
+++ Lista1Parte2/Problema0.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+  int *u= NULL, *v= NULL, *w= NULL, *q= NULL;
+  int a= 101, b= 201, c= 301;
+  u= &c; v= &b; w= &a;
+
+  (*w)+= a + b + c;
+  (*u)++;
+  (*v)*= 4;
+
+  q= w; w= u; u= v; v= q;
+
+  (*u)-= (*q)%5 - a;
+  (*v)-= (*q)%3 - b;
+  (*w)-= (*q)%2 - c;
+  printf("a=%d b=%d c= %d\n", a, b, c);
+  return 0;
+}
