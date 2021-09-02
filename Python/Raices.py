7093e29ad5abbf2d4ca5330b28f82d556af6d882
Jon BM <jbenitez.m14@gmail.com>
2021-09-01 22:41:54 -0500

    Init

---
+++ Python/Raices.py
@@ -0,0 +1,7 @@
+
+print("Programa que calcula las raices enesimas")
+
+a = float(input("Ingresa tu variable real: "))
+b = float(input("Ingresa la variable imaginaria: "))
+
+print("Tu numero complejo es: ",a)
