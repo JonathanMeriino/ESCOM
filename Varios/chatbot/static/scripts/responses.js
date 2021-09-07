f7c983f361a42c7ce617c86656de3302a3bd7f97
JonathanBM <jbenitez.m14@gmail.com>
2021-09-07 16:46:12 -0500

    cambios

---
+++ Varios/chatbot/static/scripts/responses.js
@@ -0,0 +1,28 @@
+function getBotResponse(input) {
+    //
+    if (input == "papers") {
+        return "rocks";
+    } else if (input == "rocks") {
+        return "papers";
+    } else if (input == "scissors") {
+        return "rock";
+    }
+
+    // Respuestas de bienvenida y despedida
+    if (input == "hola") {
+        return "Hola, en que puedo ayudarte?";
+    }else if (input == "Hola") {
+        return "Bienvenido, en que puedo ayudarte";
+
+    }else if (input == "bye") {
+        return "Vuelve pronto!";
+    } else if (input == "bye"){
+        return "Adios, gracias por visitarnos";
+
+    } else {
+        return "Try asking something else!";
+    }
+}
+
+
+
