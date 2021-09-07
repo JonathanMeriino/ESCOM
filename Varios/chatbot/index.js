cd3734992dcc822e378880f9f383dffa1ec40ba8
JonathanBM <jbenitez.m14@gmail.com>
2021-09-07 16:46:01 -0500

    actualizacion 16:45

--- Varios/chatbot/index.js
+++ Varios/chatbot/index.js
@@ -1,41 +1,154 @@
-
-
-
-
 const productsArroz = [
-    {producto: 'Arroz' ,place: 'CDMX', precio: 14},
-    {producto: 'Arroz' ,place: 'SanLuis', precio: 18},
-    {producto: 'Arroz' ,place: 'Campeche', precio: 21},
-    {producto: 'Arroz' ,place: 'Yucatan', precio: 33},
-    {producto: 'Arroz' ,place: 'edoMex', precio: 3},
+    {place: 'CDMX', precio: 14},
+    {place: 'SanLuis', precio: 18},
+    {place: 'Campeche', precio: 21},
+    {place: 'Yucatan', precio: 33},
+    {place: 'edoMex', precio: 3},
 ];
-
-
 const productsMantequilla = [
-    {producto: 'Mantequilla' ,lugar: 'Walmart', precio: 15},
-    {producto: 'Mantequilla' ,lugar: 'Superama', precio: 25},
-    {producto: 'Mantequilla' ,lugar: 'Bodega aurrera', precio: 20},
-    {producto: 'Mantequilla' ,lugar: 'Tienda Don Pancho', precio: 13},
-    {producto: 'Mantequilla' ,lugar: 'Waldos', precio: 30},
+    {lugar: 'Walmart', precio: 15},
+    {lugar: 'Superama', precio: 25},
+    {lugar: 'Bodega aurrera', precio: 20},
+    {lugar: 'Tienda Don Pancho', precio: 13},
+    {lugar: 'Waldos', precio: 30},
+];
+const productsLeche = [
+    {lugar: 'Walmart', precio: 15},
+    {lugar: 'Superama', precio: 25},
+    {lugar: 'Bodega aurrera', precio: 20},
+    {lugar: 'Tienda Don Pancho', precio: 13},
+    {lugar: 'Waldos', precio: 30},
+];
+const productsFrijol = [
+    {lugar: 'Walmart', precio: 15},
+    {lugar: 'Superama', precio: 25},
+    {lugar: 'Bodega aurrera', precio: 20},
+    {lugar: 'Tienda Don Pancho', precio: 13},
+    {lugar: 'Waldos', precio: 30},
+];
+const productsHuevo = [
+    {lugar: 'Walmart', precio: 15},
+    {lugar: 'Superama', precio: 25},
+    {lugar: 'Bodega aurrera', precio: 20},
+    {lugar: 'Tienda Don Pancho', precio: 13},
+    {lugar: 'Waldos', precio: 30},
+];
+const productsTortilla = [
+    {lugar: 'Walmart', precio: 15},
+    {lugar: 'Superama', precio: 25},
+    {lugar: 'Bodega aurrera', precio: 20},
+    {lugar: 'Tienda Don Pancho', precio: 13},
+    {lugar: 'Waldos', precio: 30},
+];
+const productsFrutas = [
+    {lugar: 'Walmart', precio: 15},
+    {lugar: 'Superama', precio: 25},
+    {lugar: 'Bodega aurrera', precio: 20},
+    {lugar: 'Tienda Don Pancho', precio: 13},
+    {lugar: 'Waldos', precio: 30},
+];
+const productsVerduras = [
+    {lugar: 'Walmart', precio: 15},
+    {lugar: 'Superama', precio: 25},
+    {lugar: 'Bodega aurrera', precio: 20},
+    {lugar: 'Tienda Don Pancho', precio: 13},
+    {lugar: 'Waldos', precio: 30},
+];
+const productsAceite = [
+    {lugar: 'Walmart', precio: 15},
+    {lugar: 'Superama', precio: 25},
+    {lugar: 'Bodega aurrera', precio: 20},
+    {lugar: 'Tienda Don Pancho', precio: 13},
+    {lugar: 'Waldos', precio: 30},
+];
+const productsAzucar = [
+    {lugar: 'Walmart', precio: 15},
+    {lugar: 'Superama', precio: 25},
+    {lugar: 'Bodega aurrera', precio: 20},
+    {lugar: 'Tienda Don Pancho', precio: 13},
+    {lugar: 'Waldos', precio: 30},
 ];
 
 function buscoMantequilla() {
     
-    const wantMantequilla = productsMantequilla.filter(x => x.producto == 'Mantequilla' & x.precio <20 & x.lugar == 'Walmart')
+    const wantMantequilla = productsMantequilla.filter(x => x.precio <20 & x.lugar == 'Walmart')
 
     return console.log(wantMantequilla);
-}
-
-buscoMantequilla();
+} 
 
 function buscoArroz(){
 
-    const wantArroz = productsArroz.filter(x => x.producto == 'Arroz' & x.precio < 15 & x.place == 'CDMX')
+    const wantArroz = productsArroz.filter(x => x.precio < 15 & x.place == 'CDMX')
     
     return console.log(wantArroz);
+} 
+function buscoLeche(){
 
+    const wantLeche = productsLeche.filter(x => x.precio <20 & x.lugar == 'Walmart')
+
+    return console.log(wantLeche);
+}
+function buscoFrijol(){
+
+    const wantFrijol = productsFrijol.filter(x => x.precio <20 & x.lugar == 'Walmart')
+
+    return console.log(wantFrijol);
+}
+function buscoHuevo(){
+
+    const wantHuevo = productsHuevo.filter(x => x.precio <20 & x.lugar == 'Walmart')
+
+    return console.log(wantHuevo);
+}
+function buscoTortilla(){
+
+    const wantTortilla = productsTortilla.filter(x => x.precio <20 & x.lugar == 'Walmart')
+
+    return console.log(wantTortilla);
+}
+function buscoFruta(){
+
+    const wantFruta = productsFruta.filter(x => x.precio <20 & x.lugar == 'Walmart')
+
+    return console.log(wantFruta);
+}
+function buscoVerdura(){
+
+    const wantVerdura = productsVerduras.filter(x => x.precio <20 & x.lugar == 'Walmart')
+
+    return console.log(wantVerdura);
+}
+function buscoAzucar(){
+
+    const wantAzucar = productsAzucar.filter(x => x.precio <20 & x.lugar == 'Walmart')
+
+    return console.log(wantAzucar);
+}
+function buscoAceite(){
+
+    const wantAceite = productsAceite.filter(x => x.precio <20 & x.lugar == 'Walmart')
+
+    return console.log(wantAceite);
 }
 
-buscoArroz();
-
-
+if (input == "busco mantequilla") {
+    return buscoMantequilla();
+} else if (input == "busco arroz") {
+    return buscoArroz();
+} else if (input == "busco huevo") {
+    return buscoHuevo();
+} else if (input == "busco leche"){
+    return buscoLeche();
+}else if (input == "busco Tortilla"){
+    return buscoTortilla();
+}else if (input == "busco frijol"){
+    return buscoFrijol();
+}else if (input == "busco frutas"){
+    return buscoFruta();
+}else if (input == "busco verduras"){
+    return buscoVerduras();
+}else if (input == "busco aceite"){
+    return buscoAceite();
+}else if (input == "busco azucar"){
+    return buscoAzucar();
+}
