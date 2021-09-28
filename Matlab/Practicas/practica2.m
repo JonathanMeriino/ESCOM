536d020759cafbacdfcfefa865ec49e40b460226
LeoMerino <jbenitez.m14@gmail.com>
2021-09-28 16:51:37 -0500

    comentarios practica 2

--- Matlab/Practicas/practica2.m
+++ Matlab/Practicas/practica2.m
@@ -1,108 +1,100 @@
+%Practica 2 - Expansion del histograma
 clc
 clear all
 close all
 warning of all
 
 b=imread('peppers.png');
-%figure(3)
-%imshow(b);
-%imhist(b);
 
 roja=b;
-roja(:,:,1);
-roja(:,:,2)=0;
-roja(:,:,3)=0;
-%imshow(roja);
+roja(:,:,[2 3])=0;
 
 verde=b;
-verde(:,:,1)=0;
-verde(:,:,2);
-verde(:,:,3)=0;
-%imshow(verde);
+verde(:,:,[1 3])=0;
 
 azul=b;
-azul(:,:,1)=0;
-azul(:,:,2)=0;
-azul(:,:,3);
-%imshow(azul);
+azul(:,:,[1 2])=0;
 
 figure(1)
-roja_r=roja(:,:,1);
+% imagen roja antes de expansion
+rojaGris=roja(:,:,1); %roja escala de grises
 subplot(3,3,1)
 imshow(roja);
-title('imagen roja');
+title('Imagen roja');
 subplot(3,3,4)
-imshow(roja_r);
-title('imagen gris roja');
+imshow(rojaGris); 
+title('Imagen gris-roja');
 subplot(3,3,7)
-histogram(roja_r);
-title('histograma gris roja');
-
-verde_r=verde(:,:,2);
+histogram(rojaGris);
+title('Histograma gris-roja');
+%imagen verde antes de expansion
+verdeGris=verde(:,:,2); %verde escala de grises
 subplot(3,3,2)
 imshow(verde);
-title('imagen verde');
+title('Imagenverde');
 subplot(3,3,5)
-imshow(verde_r);
-title('imagen gris verde')
+imshow(verdeGris);
+title('Imagen gris-verde')
 subplot(3,3,8)
-histogram(verde_r);
-title('histograma gris verde')
-
-azul_r=azul(:,:,3);
+histogram(verdeGris);
+title('Histograma gris-verde')
+%imagen azul antes de expansion
+azulGris=azul(:,:,3); %azul escala de grises
 subplot(3,3,3)
 imshow(azul);
-title('imagen azul');
+title('Imagen azul');
 subplot(3,3,6)
-imshow(azul_r);
-title('imagen gris azul');
+imshow(azulGris);
+title('Imagen gris-azul');
 subplot(3,3,9)
-histogram(azul_r);
-title('histograma gris azul');
-
-MIN = 0;
+histogram(azulGris);
+title('Histograma gris-azul');
+%minimo y maximos deseados
+MIN = 0; 
 MAX = 255;
 opcion = 1;
 
 while opcion == 1
+% minimo y maximo añadidos por el usuario
 minimo = input('Ingrese valor minimo: ');
 maximo= input('Ingrese valor maximo: ');
-
+%formula para calcular la expancion del histogrma
 variableX= (MAX - MIN);
 variableY= (maximo - minimo);
 variableZ= (double(variableY)/double(variableX));
-
+%recorrido de i filas y j columnas
 for i = 1:384
     for j = 1:512
-        new_roja(i,j) = (roja_r(i,j)-MIN)*variableZ + minimo;
+        expRoja(i,j) = (rojaGris(i,j)-MIN)*variableZ + minimo; % nueva imagen roja
         
-        new_verde(i,j) = (verde_r(i,j)-MIN)*variableZ + minimo;
-        
-        new_azul(i,j) = (azul_r(i,j)-MIN)*variableZ + minimo;
+        expVerde(i,j) = (verdeGris(i,j)-MIN)*variableZ + minimo; % nueva imagen verde
+         
+        expAzul(i,j) = (azulGris(i,j)-MIN)*variableZ + minimo; %nueva imagen azul
     end
 end
-figure(2)
-%R
-subplot(2,3,1);
-imshow(new_roja);
-title('imagen roja new');
-subplot(2,3,4);
-histogram(new_roja);
-title('histograma rojo new');
-%G
-subplot(2,3,2);
-imshow(new_verde);
-title('imagen verde new');
-subplot(2,3,5);
-histogram(new_verde);
-title('histograma verde new');
 
-%B
+figure(2)
+%Red
+subplot(2,3,1);
+imshow(expRoja);
+title('Nueva imagen R');
+subplot(2,3,4);
+histogram(expRoja);
+title('Nuevo Histograma R');
+%Green
+subplot(2,3,2);
+imshow(expVerde);
+title('Nueva imagen G');
+subplot(2,3,5);
+histogram(expVerde);
+title('Nueva Histograma G');
+%Blue
 subplot(2,3,3);
-imshow(new_azul);
-title('imagen azul new');
+imshow(expAzul);
+title('Nueva imagen B');
 subplot(2,3,6);
-histogram(new_azul);
-title('histograma azul new');
+histogram(expAzul);
+title('Nueva Histograma B');
+
 opcion = input('Desea comprimir o expandir nuevamente (1.- SI, 2.- NO):');
 end
