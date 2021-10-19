8f303d22033e5b65e914dd100502c8a01294c27e
LeoMerino <jbenitez.m14@gmail.com>
2021-10-19 17:38:24 -0500

    mysql conexion

---
+++ Projects/Gestor/src/database.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql');
+
+const {promisify} =require('util');
+const {database} = require('./keys');
+
+const pool = mysql.createPool(database);
+
+pool.getConnection((err, connection)=>{
+    if(err){
+        if(err.code=== 'PROTOCOL_CONNECTION_LOST'){
+            console.error('DATABASE CONNECTION WAS CLOSED');
+        }
+        if(err.code ==='ER_CON_COUNT:ERROR'){
+            console.error('DATABASE HAS TO MANY CONNECTIONS');
+        }
+        if(err.code === 'ECONNREFUSE'){
+            console.error('DATABASE CONNECTION WAS REFUSED');
+        }
+    }
+    if(connection) connection.release();
+    HTMLFormControlsCollection.log('DB is Connected');
+    return;
+});
+// Promisify pool querys
+pool.query = promisify(pool.query);
+
+module.exports = pool;
---
+++ Projects/Gestor/src/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    database: {
+        host: 'localhost',
+        user: 'tobi',
+        password: 'Tobi.escom',
+        database: 'database_links'
+    }
+
+}
