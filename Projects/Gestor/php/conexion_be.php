0b20ecfe58da325adc3197d39b0ec934e535868d
LeoMerino <jbenitez.m14@gmail.com>
2021-12-01 23:33:38 -0600

    update

---
+++ Projects/Gestor/php/conexion_be.php
@@ -0,0 +1,12 @@
+<?php
+    $conexion = mysqli_connect("localhost","root","Tobi.escom","login_registro");
+/*
+    if($conexion ){
+        echo 'Conectado exitosamente';
+    }else{
+        echo 'No se ha podido conectar';
+    }
+*/
+
+
+?>
