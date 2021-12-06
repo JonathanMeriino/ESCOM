5c97cfbe144d5314dd5a0db993f22af26fed3106
LeoMerino <jbenitez.m14@gmail.com>
2021-12-05 20:08:42 -0600

    update

--- Projects/Clasificador/main.py
+++ Projects/Clasificador/main.py
@@ -1,24 +1,27 @@
 import pandas as pd
-#from imblearn.under_sampling import RandomUnderSampler
-#from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.preprocessing import OneHotEncoder
+from imblearn.under_sampling import RandomUnderSampler
+from sklearn.feature_extraction.text import TfidfVectorizer
+#from sklearn.preprocessing import OneHotEncoder
 
 datos= pd.read_csv('grimms.csv', low_memory=False)    # lectura de archivo csv
 #print(datos)
 print(datos.dtypes)  # tipos de datos
-
-print(datos.describe())
-print(datos.info)
-#print(datos)
+print(datos)
 
 #Preprocesamiento
+
 #visualizar dataframes
 #print(datos.head(1))
 #print(datos.tail(1))
-
+#print(datos.describe())
+#print(datos.info)
 datos_pre = datos.drop(['Title','Count'], axis=1)
-#print(datos_pre)
-print(datos_pre.dtypes)
+print(datos_pre)
+#print(datos_pre.dtypes)
+
+#beta=pd.get_dummies(datos,columns = ['Title']) -> cambiar de variables categoricas a numericas
+
+# 
 
 #datosPreOne = OneHotEncoder(datos_pre = [0])
 #x = datosPreOne.fit_transform(datosPreOne).toArray()
@@ -33,9 +36,9 @@ print(datos_pre.dtypes)
 
 #tfidf = TfidfVectorizer(stop_words='english')
 
-#cuentosVector =tfidf.fit_transform(cuentos) #encontrar los parametros ideales para la informacion y aplicarlos
+#cuentosVector =tfidf.fit_transform(datos_pre) #encontrar los parametros ideales para la informacion y aplicarlos
 
-
+#print(cuentosVector.dtypes)
 
 # finalizacion
 #datos_pre.to_csv("datos_clasificados.csv", index =False)
@@ -52,5 +55,19 @@ Crear entorno virtual en conda:
     3. conda install pandas
         conda install scikit-learn
         conda install imblearn
+
+"""
+
+"""
+-Eliminar datos faltantes
+
+axis= 0 - para elimiar filas 
+axis = 1 - para elimiar columnas
+
+df.dropna()
+
+- Remplazar datos faltantes
+
+raplece (data a reemplazar, nuevo dato)
 
 """
