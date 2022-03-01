7c5c43e89ab131413978d31a57ed95c8a0640eb4
LeoMerino <jbenitez.m14@gmail.com>
2022-02-28 21:19:05 -0600

    Operaciones con lenguajes

--- Projects/OperacionesLenguaje/opLenguajes.py
+++ Projects/OperacionesLenguaje/opLenguajes.py
@@ -1,4 +1,7 @@
 #ingreso de las cadenas
+import re
+
+
 def introduceLenguages(lenguage,len,index):
     if len==0:
         lenguage=[]
@@ -51,8 +54,25 @@ def difference(lenguage1, lenguage2, index1, index2, dif):
     if index1<len(lenguage1)-1:
         difference(lenguage1, lenguage2, index1+1, index2, dif)
 
-def potencia(): 
-    return 0
+def potencia():
+
+    pot = int(input("Ingrese el valor de la potencia: "))
+    cadena = input("Ingrese cadena: ")
+    pote=[]
+
+    for line in range(pot+1):
+        if line == 0:
+            aux="E"
+            pote.append(aux)
+        
+        elif line>0 and line<10:
+            aux=cadena*line
+            pote.append(aux)
+    print("Potencia de la cadena: ", pote, sep='\n')
+
+    return '\n'
+    
+    
 
 def concatenar(lenguage1,lenguage2,index1, index2,concat):
     if lenguage1[index1]!=lenguage2[index2]: #si lenguaje1(i1) es diferente a lenguaje2[i2]
@@ -71,21 +91,51 @@ def concatenar(lenguage1,lenguage2,index1, index2,concat):
     if index2<len(lenguage2)-1:             #si (i2) es menor a longitud de lenguaje2-12
         concatenar(lenguage1, lenguage2, index1, index2+1, concat)
 
-def cerrKleene(lenguage):
+def cerrKleene():
     
-    
-    return lenguage   
+    n=10
+    cadena = input("Ingrese cadena: ")
+    klenne=[]
 
+    for line in range(n+1):
+        if line == 0:
+            aux="E"
+            klenne.append(aux)
+        
+        elif line>0 and line<10:
+            aux=cadena*line
+            klenne.append(aux)
+    klenne.append('...')
+    print("Cerradura de Kleene",klenne, sep='\n')
+
+    return '\n'
+        
+    
 def cerrPos():
-    return 0
+    
+    n=10
+    cadena = input("Ingrese cadena: ")
+    pos=[]
+
+    for line in range(1,n+1):
+        aux=cadena*line
+        pos.append(aux)
+    pos.append('...')
+    print("Cerradura Positiva",pos, sep='\n')
+
+    return '\n'
 
 
-lenguage1 , lenguage2=[]
-inverso1, inverso2=[]
+lenguage1 =[]
+
+lenguage2=[]
+inverso1=[]
+inverso2=[]
 intersec=[]
 uni=[]
 concat = []
-diferencia1,diferencia2=[]
+diferencia1=[]
+diferencia2=[]
 kleene1=[]
 kleene2=[]
 
@@ -105,8 +155,8 @@ union(lenguage1, lenguage2, 0,0, uni)
 difference(lenguage1, lenguage2, 0, 0, diferencia1)
 difference(lenguage2, lenguage1, 0, 0, diferencia2)
 
-cerrKleene(lenguage1)
-cerrKleene(lenguage2)
+#cerrKleene(lenguage1,kleene1)
+#cerrKleene(lenguage2,kleene2)
 
 concatenar(lenguage1, lenguage2, 0,0,concat)
 
@@ -126,17 +176,24 @@ print("La diferencia tipo A-B es:", diferencia1, sep='\n')
 
 print("La diferencia tipo B-A es:", diferencia2, sep='\n')
 
+#len1=int(input("Ingresa el valor de la potencia"))
+#print("Potencia de lenguaje: ", potencia , sep='\n')
+
+concat = list(dict.fromkeys(concat))
+
 print("La concatenacion es:", concat , sep='\n')
 
-print("Cerradura de Kleene del primer lenguaje es: ")
+#print("Cerradura de Kleene del primer lenguaje es: ",cerrKleene, sep='\n')
 
-print("Cerradura de Kleene del segundo lenguaje es: ")
+#print("Cerradura de Kleene del segundo lenguaje es: ")
 
+#print(cerrKleene(lenguage1))
 
+#print("Cerradura Positiva: ", cerrPos(lenguage1))
 
-print("Cerradura Positiva: ")
+print(potencia())
 
-print("Concatenacion: ")
+print(cerrKleene())
 
-print("Potencia de lenguaje: ")
+print(cerrPos())
 
