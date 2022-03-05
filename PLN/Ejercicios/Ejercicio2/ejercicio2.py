4a6e53831ba8395623cdde6a1003452dde827779
LeoMerino <jbenitez.m14@gmail.com>
2022-03-04 21:27:25 -0600

    TERMINADO

--- PLN/Ejercicios/Ejercicio2/ejercicio2.py
+++ PLN/Ejercicios/Ejercicio2/ejercicio2.py
@@ -1,40 +1,51 @@
+"""
+	Benitez Merino Leonardo Jonathan
+	5BM1
+"""
+#importacion de bibliotecas
 import numpy as np
 import pandas as pd
 import re
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from spacy.lang.es.stop_words import STOP_WORDS 
 
+#lectura del corpus
 with open ('corpusFinal.txt') as corpusEntrada:
 	dataset = corpusEntrada.read()
 
-dataset = re.split('\n',dataset)
-
-
+dataset = re.split('\n',dataset) # conversion del dataset en una lista
 
 # Representación vectorial binarizada
 vectorizador_binario = CountVectorizer(binary=True)
 vec_binarizado = vectorizador_binario.fit_transform(dataset)
-print (vectorizador_binario.get_feature_names_out())
-print (vec_binarizado)#sparse matrix
+features=vectorizador_binario.get_feature_names_out()
+#print(features)
+#print(len(features))
+#print (vec_binarizado)#sparse matrix
 print (type(vec_binarizado.toarray()))#dense ndarray
 print ('Representación vectorial binarizada')
-print (vec_binarizado.toarray())#dense ndarray
-
+#print (vec_binarizado.toarray())#dense ndarray
+print("Tamaño matriz: ",vec_binarizado.shape)
+df_bin = pd.DataFrame.sparse.from_spmatrix(vec_binarizado,columns=[features])
+#print(df_bin)
+#df_bin.to_csv('representacion_binarizada.txt',header=True,index=False, sep='\t')
 
 #~ #Representación vectorial por frecuencia
 vectorizador_frecuencia = CountVectorizer()
-X = vectorizador_frecuencia.fit_transform(dataset)
+vec_frec = vectorizador_frecuencia.fit_transform(dataset)
 print('Representación vectorial por frecuencia')
-print (X.toarray())
-
+#print (vec_frec.toarray())
+print("Tamaño matriz: ",vec_frec.shape)
+df_frec = pd.DataFrame.sparse.from_spmatrix(vec_frec,columns=[features])
+#print(df_frec)
+#df_frec.to_csv('representacion_frecuencia.txt',header=True,index=False, sep='\t')
+#Representación vectorial tf-idf
 vectorizador_tfidf = TfidfVectorizer()
-X = vectorizador_tfidf.fit_transform(dataset)
+vec_tfidf = vectorizador_tfidf.fit_transform(dataset)
 print ('Representación vectorial tf-idf')
-print (X.toarray())
+#print (vec_tfidf.toarray())
+print("Tamaño matriz: ",vec_tfidf.shape)
+#dataframe
+df_tfidf = pd.DataFrame.sparse.from_spmatrix(vec_tfidf,columns=[features])
+df_tfidf.to_csv('df_tfidf.txt',header=True,index=False, sep='\t')
 
-print(X.shape)
-
-#Pandas
-df = pd.DataFrame.sparse.from_spmatrix(X, columns=[len(dataset)])
-print(df)
