2c96e7c240f7f719b456926f0f2cdb3a64a8b12e
LeoMerino <jbenitez.m14@gmail.com>
2022-03-25 12:07:22 -0600

    update

--- PLN/Practicas/Practica4/practica4.py
+++ PLN/Practicas/Practica4/practica4.py
@@ -17,7 +17,7 @@ tagged_data = [TaggedDocument(d, [i]) for i, d in enumerate(datasetToken)]
 #model01005.save("doc2vec.modelToken01005") 
 ## Load saved doc2vec model
 model01005= Doc2Vec.load("doc2vec.modelToken01005")
-print("Modelo Dataset Tokenizado: dm[0],vector_size[100] , window[5]",model01005.dv.most_similar(model01005.dv[104]),sep='\n')
+print("Modelo Dataset Tokenizado: dm[0],vector_size[100] , window[5]",model01005.dv.most_similar(model01005.dv[300]),sep='\n')
 
 
 #Combinacion dm[0] , vector_size[100] , window[10]
@@ -26,71 +26,67 @@ print("Modelo Dataset Tokenizado: dm[0],vector_size[100] , window[5]",model01005
 #Guardar modelo
 #model010010.save("doc2vec.modelToken010010") 
 ## Load saved doc2vec model
-#model010010= Doc2Vec.load("doc2vec.modelToken010010")
-#print("Modelo Dataset Tokenizado: dm[0],vector_size[100] , window[10]",model010010.dv.most_similar(model010010.dv[384]),sep='\n')
+model010010= Doc2Vec.load("doc2vec.modelToken010010")
+print("Modelo Dataset Tokenizado: dm[0],vector_size[100] , window[10]",model010010.dv.most_similar(model010010.dv[87]),sep='\n')
 
 
 #Combinacion dm[0] , vector_size[300] , window[5]
 #model03005 = Doc2Vec(tagged_data,dm=0, vector_size=300,window=5)
-"""
 #Guardar modelo
-model03005.save("doc2vec.modelToken03005") 
+#model03005.save("doc2vec.modelToken03005") 
 ## Load saved doc2vec model
 model03005= Doc2Vec.load("doc2vec.modelToken03005")
+print("Modelo Dataset Tokenizado: dm[0],vector_size[300] , window[5]",model03005.dv.most_similar(model03005.dv[10]),sep='\n')
 
-"""
 
 #Combinacion dm[0] , vector_size[300] , window[10]
 #model030010 = Doc2Vec(tagged_data,dm=0, vector_size=300,window=10)
-"""
+
 #Guardar modelo
-model030010.save("doc2vec.modelToken030010") 
+#model030010.save("doc2vec.modelToken030010") 
 ## Load saved doc2vec model
 model030010= Doc2Vec.load("doc2vec.modelToken030010")
-
-"""
+print("Modelo Dataset Tokenizado: dm[0],vector_size[300] , window[10]",model030010.dv.most_similar(model030010.dv[84]),sep='\n')
 
 #Combinacion dm[1] , vector_size[100] , window[5]
 #model11005 = Doc2Vec(tagged_data,dm=1, vector_size=100,window=5)
-"""
+
 #Guardar modelo
-model11005.save("doc2vec.modelToken11005") 
+#model11005.save("doc2vec.modelToken11005") 
 ## Load saved doc2vec model
 model11005= Doc2Vec.load("doc2vec.modelToken11005")
-
-print(model.dv.most_similar(model.dv[3]))
-"""
+print("Modelo Dataset Tokenizado: dm[1],vector_size[100] , window[5]",model11005.dv.most_similar(model11005.dv[34]),sep='\n')
 
 #Combinacion dm[1] , vector_size[100] , window[10]
 #model110010 = Doc2Vec(tagged_data,dm=1, vector_size=100,window=10)
-"""
+
 #Guardar modelo
-model110010.save("doc2vec.modelToken110010") 
+#model110010.save("doc2vec.modelToken110010") 
 ## Load saved doc2vec model
 model110010= Doc2Vec.load("doc2vec.modelToken110010")
+print("Modelo Dataset Tokenizado: dm[1],vector_size[100] , window[10]",model110010.dv.most_similar(model110010.dv[478]),sep='\n')
 
-"""
 
 #Combinacion dm[1] , vector_size[300] , window[5]
 #model13005 = Doc2Vec(tagged_data,dm=1, vector_size=300,window=5)
-"""
+
 #Guardar modelo
-model13005.save("doc2vec.modelToken13005") 
+#model13005.save("doc2vec.modelToken13005") 
 ## Load saved doc2vec model
 model13005= Doc2Vec.load("doc2vec.modelToken13005")
+print("Modelo Dataset Tokenizado: dm[1],vector_size[300] , window[5]",model13005.dv.most_similar(model13005.dv[212]),sep='\n')
 
-"""
 
 
 #Combinacion dm[1] , vector_size[300] , window[10]
 #model130010 = Doc2Vec(tagged_data,dm=1, vector_size=300,window=10)
-"""
+
 #Guardar modelo
-model130010.save("doc2vec.modelToken130010") 
+#model130010.save("doc2vec.modelToken130010") 
 ## Load saved doc2vec model
 model130010= Doc2Vec.load("doc2vec.modelToken130010")
+print("Modelo Dataset Tokenizado: dm[1],vector_size[300] , window[10]",model130010.dv.most_similar(model130010.dv[105]),sep='\n')
 
-"""
 
 """"
 Modelos para Corpus Practica 2
