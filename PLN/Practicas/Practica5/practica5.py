07bcca0e080945a37171032df0ccab0a9b8ccc88
JonathanBM <jbenitez.m14@gmail.com>
2022-04-01 21:31:44 -0600

    completo

--- PLN/Practicas/Practica5/practica5.py
+++ PLN/Practicas/Practica5/practica5.py
@@ -1,56 +1,34 @@
-import spacy
 import re
 import spacy
 import pandas as pd
-#from rake_nltk import Rake
-from spacy.lang.es.stop_words import STOP_WORDS
+from rake_nltk import Rake
 
-nlp = spacy.load('es_core_news_sm') #pipeline para el preprocesamiento
-nlp.max_length = 1600000 # longitud del texto
 
-with open ('corpusStopLimpio.txt') as corpusEntrada:
+
+with open ('corpusStopLimpio.txt',encoding="utf8") as corpusEntrada:
 	corpus = corpusEntrada.read()
 	
 
 corpus = re.split("\n", corpus)
-print(corpus[1])
 
-"""r = Rake()
-noticia1 = corpus[0]
 
-print(noticia1)
-
-print(r.extract_keywords_from_text(noticia1))
-print(r.get_ranked_phrases())
-
-ranks=[r.get_ranked_phrases_with_scores()]
-print(r.get_ranked_phrases_with_scores())
-print(type(ranks))"""
-#doc = nlp(corpus)
-
-#tokens = [token for token in doc if not token.is_stop]
-
-#tokens = []
-#
-#for token in doc:
-#	if not token.is_stop:
-#		tokens.append(token)
-#print(tokens)
+r = Rake()
 		
-"""with open('corpusStop.txt', 'w') as doc:
-	for i in tokens:
-		doc.write("%s\n" %i)
-tokens.close()"""
-"""
 indices = []
-
+listaRanks = []
 for i in corpus:
-	indices.append(i)
+    r.extract_keywords_from_text(i)
+    listaRanks.append(r.get_ranked_phrases_with_scores())
+    indices.append(i)
+    
 
 dataframe = pd.DataFrame()
 
 
-dataframe['Noticia'] = indices[0:1]
-dataframe['Palabras Clave'] = ranks
+dataframe['Noticia'] = indices
+dataframe['Palabras Clave'] = listaRanks
 
-print(dataframe)"""
+dataframe1= dataframe
+print(dataframe)
+
+dataframe.to_csv("TablaCompleta.csv")
