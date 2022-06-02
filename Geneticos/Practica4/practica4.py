d72bd1981ecdcc355fc126749beba4cfb1497c22
LeoMerino <jbenitez.m14@gmail.com>
2022-06-02 18:43:32 -0500

    update

---
+++ Geneticos/Practica4/practica4.py
@@ -0,0 +1,64 @@
+"""
+Benitez Merino Leonardo Jonathan
+Realizar la minimizacion de la funcion x**2 + y**2 en el intervalo de valores 
+(-5,5) para x y y
+version de PSO: Global
+Numero de particulas: 20
+iteraciones: 50
+w = 0.8 -> inercia
+c1 = 0.7
+c2=1 
+"""
+from turtle import clear
+import numpy as np
+import pandas as pd
+datos = pd.DataFrame()
+
+
+particulas = 20
+dimension = 5
+arr = 0
+"""¿¿def crearParticulas(particulas, dimension, arr):
+    arr = np.random.rand(particulas, dimension)
+    print(arr)
+
+    return""" 
+
+arr = np.random.uniform(low=-5,high=5,size=(particulas,dimension))
+#print(arr)
+#print(arr[0])
+P=[]
+
+for i,j in enumerate(arr):
+    aux=j
+    P.append(aux)
+
+#print(P)
+gbest = [0,0,0,0,0]
+
+
+#res = sum(map(lambda i : i * i, P))
+
+#print(res)
+eval_P=[]
+
+for i in P:
+    aux = sum(i*i)
+    eval_P.append(aux)
+
+#print(eval_P)
+
+# seleccionar el gbest 
+print("Valor maximo: ", max(eval_P))
+print("Valor minimo: ", min(eval_P))
+
+"""def eval_OF(P):
+    eval_P = []
+    for i in P:
+        break    
+    return 0"""
+
+
+datos['Particulas']=P
+datos ['Eval_P']= eval_P
+print(datos)
