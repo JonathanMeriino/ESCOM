55167dbd78d6920ed41a228c91680a5b258ebe86
JonathanBM <jbenitez.m14@gmail.com>
2022-06-25 23:45:43 -0500

    delete

--- Projects/chatbot/Shoppybot/static/script/responses.js
+++
@@ -1,8 +0,0 @@
-function getBotResponse(input)
-{
-    if(input == "hola")
-        return "holi";
-    else if(input==" ")
-        return "No entiendo a lo que te refieres. Escribe cancelar, si deseas cancelar la busqueda.";
-        
-}
