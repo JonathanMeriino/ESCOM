8013e3c3f117d0ed4527b3cd113b0683d1db7e80
Jonathan-Ubuntu <jbenitez.m14@gmail.com>
2023-01-06 12:50:19 -0600

    edit

--- Varios/correlacion.py
+++ Varios/correlacion.py
@@ -10,12 +10,12 @@ import numpy as np
 ecg = electrocardiogram()
 fs= 360
 time = np.arange(ecg.size) / fs
-plt.figure()
+plt.figure(1)
 plt.plot(time,ecg)
 plt.xlim(9, 10.2)
 plt.ylim(-1, 1.5)
 plt.show()
 correlacion=np.correlate(ecg,ecg)
-plt.figure()
+#plt.figure()
 plt.xcorr(ecg,ecg)
 plt.show()
